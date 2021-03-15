4c701fcfae1aaad70030d87e97fdcd664e0a4e7b
Gzim <gzimasanni@gmail.com>
2021-03-15 10:47:53 +0100

    Add calculations

--- app.js
+++ app.js
@@ -12,6 +12,22 @@ function calculateResults(e) {
   const DtotalPayment = document.getElementById('total-payment');
   const DtotalInterest = document.getElementById('total-interest');
  
+  const principal = parseFloat(Damount.value);
+  const calculatedInterest = parseFloat(Dinterest.value)/100/12;
+  const calculatedPayments = parseFloat(Dyears.value)*12;
+
+  //Compute monthly payments
+
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal*x*calculatedInterest)/(x-1);
+
+  if(isFinite(monthly)) {
+    DmonthlyPayment.value = monthly.toFixed(2);
+    DtotalPayment.value = (monthly*calculatedPayments).toFixed(2);
+    DtotalInterest.value = ((monthly * calculatedPayments) - principal.toFixed(2))
+  } else {
+    console.log("Wrong!!!!!!")
+  }
 
   console.log('calculating')
   e.preventDefault()
